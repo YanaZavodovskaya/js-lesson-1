7a61e18103aec92cb321095127a452f8959b5d32
YanaZavodovskaya <104728312+YanaZavodovskaya@users.noreply.github.com>
2023-04-03 22:25:11 +0600

    conspect-1

---
+++ js/script-1.js
@@ -0,0 +1,6 @@
+const a = 'а из скрипта-1';
+console.log(a);
+
+const b = 'b из скрипта-1';
+console.log(b);
+
---
+++ js/script-2.js
@@ -0,0 +1,5 @@
+const a = 'а из скрипта-2';
+console.log(a);
+
+const c = 'c из скрипта-1';
+console.log(c);
